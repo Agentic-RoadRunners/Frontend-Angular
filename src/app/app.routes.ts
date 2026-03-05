f2c6bb9aaf22bf3e052d775863d6990ae859b362
Altuğ İkiz <altugikiz10@gmail.com>
2026-03-05 14:37:24 +0300

    feat: updated role guard authentications

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,7 @@ export const routes: Routes = [
   { path: 'home', loadComponent: () => import('./core/home/page/home-page').then(m => m.HomePage) },
   { path: 'maps', canActivate: [authGuard], loadComponent: () => import('./core/maps/page/maps-page').then(m => m.MapsPage) },
   { path: 'incidents', canActivate: [authGuard], loadComponent: () => import('./core/incidents/page/incidents-page').then(m => m.IncidentsPage) },
-  { path: 'analytics', canActivate: [authGuard], loadComponent: () => import('./core/analytics/page/analytics-page').then(m => m.AnalyticsPage) },
+  { path: 'analytics', canActivate: [authGuard, roleGuard('Admin', 'Municipality')], loadComponent: () => import('./core/analytics/page/analytics-page').then(m => m.AnalyticsPage) },
   { path: 'profile', canActivate: [authGuard], loadComponent: () => import('./core/profile/page/profile-page').then(m => m.ProfilePage) },
   { path: 'municipality', canActivate: [authGuard, roleGuard('Municipality', 'Admin')], loadComponent: () => import('./core/municipality/page/municipality-page').then(m => m.MunicipalityPage) },
   { path: 'admin', canActivate: [authGuard, roleGuard('Admin')], loadComponent: () => import('./core/admin/page/admin-page').then(m => m.AdminPage) },
